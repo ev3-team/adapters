7d300e878bd04b3ebe84a41f7c0b539ced8e0031
Wilmer Cantillo <will.canti2697@gmail.com>
2023-11-08 18:33:29 -0500

    chore: fix chain causing type error.

--- projects/sarcophagus/index.ts
+++ projects/sarcophagus/index.ts
@@ -2,7 +2,7 @@ import { AdapterProject } from '../types'
 
 export default {
   name: 'Sarcophagus',
-  chain: 'Ethereum',
+  chain: 'ETHEREUM',
   category: 'OTHER',
   token: 'SARCO',
   coingeckoId: 'sarcophagus',
