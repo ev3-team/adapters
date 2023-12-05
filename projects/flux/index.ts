751023e5baa488c77572cbdef3a481b92c34898d
The Brit <djsearle1977@gmail.com>
2023-12-05 14:15:23 +0000

    Update index.ts
    Updated the various links, chain, and description of the Flux project

--- projects/flux/index.ts
+++ projects/flux/index.ts
@@ -2,18 +2,18 @@ import { AdapterProject } from '../types'
 
 export default {
   name: 'Flux',
-  chain: 'ETHEREUM',
+  chain: 'NATIVE',
   category: 'COMPUTE',
   token: 'FLUX',
-  coingeckoId: 'zelcash',
+  coingeckoId: 'flux-zelcash',
   id: '0t3lhn',
   description:
-    'Flux is a decentralized compute network for cross-platform blockchain applications.',
+    'Flux is a decentralized Web3 cloud infrastructure comprised of user-operated, scalable and globally distributed computational nodes.',
   investors: [],
   duneQueries: null,
   blog: 'https://fluxofficial.medium.com/',
-  github: 'https://github.com/fluxcd/flux',
-  telegram: 'https://t.me/careerflux',
+  github: 'https://github.com/RunOnFlux',
+  telegram: 'https://t.me/runonflux',
   twitter: 'https://twitter.com/RunOnFlux?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor',
   url: 'https://runonflux.io/',
 } satisfies AdapterProject
