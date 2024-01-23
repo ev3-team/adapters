2497a560dd0a642cedfc32b3aed2b9572cdd25d2
stayForward09 <daveescott0059@gmail.com>
2024-01-23 13:15:30 -0400

    add new project - Edge

---
+++ projects/edge/index.ts
@@ -0,0 +1,25 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Edge',
+  ninja: null,
+  subcategories: null,
+  chain: 'NATIVE',
+  category: 'BLOCKCHAIN_INFRA',
+  token: 'EDGE',
+  coingeckoId: 'edge',
+  id: 'gsez5y',
+  description:
+    'Edge is the infrastructure of Web3. A peer-to-peer network and blockchain providing high performance decentralised web services, powered by the spare capacity all around us.',
+  discord: 'https://discord.com/invite/edge-network',
+  investors: [],
+  linkedin: null,
+  duneQueries: null,
+  foundingYear: null,
+  blog: null,
+  github: 'https://github.com/edge',
+  telegram: 'https://t.me/edgenetwork',
+  twitter: 'https://twitter.com/edgenetwork',
+  url: 'https://edge.network/',
+  verified: false,
+} satisfies AdapterProject
