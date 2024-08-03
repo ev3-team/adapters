fa0287b9c079b3dbdd0afb99eb52754549375686
meta-lite <nick@myceliumnetworks.com>
2024-08-03 13:04:03 -0500

    Add projects/sunriselayer

---
+++ projects/sunriselayer/index.ts
@@ -0,0 +1,26 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Sunrise Layer',
+  ninja: 'meta-light',
+  subcategories: 'Data',
+  chain: null,
+  category: 'COMPUTE',
+  token: 'RISE',
+  coinGeckoID: null,
+  id: 'j7kp4i',
+  cmcId: null,
+  description: null,
+  discord: 'sunrise',
+  investors: [],
+  linkedin: '',
+  duneQueries: null,
+  foundingYear: '2022',
+  blog: 'http://sunriselayer.medium.com',
+  github: 'https://github.com/sunriselayer',
+  telegram: null,
+  twitter: 'https://x.com/SunriseLayer',
+  url: 'http://sunriselayer.io',
+  verified: false,
+  fundraises: [],
+} satisfies AdapterProject
