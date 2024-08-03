29368e36048f880c94fd0d9bd8676466670148bc
meta-lite <nick@myceliumnetworks.com>
2024-08-03 13:04:36 -0500

    Add projects/tari

---
+++ projects/tari/index.ts
@@ -0,0 +1,31 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Tari',
+  ninja: 'meta-light',
+  subcategories: 'L1/L2',
+  chain: 'NATIVE',
+  category: 'BLOCKCHAIN_INFRA',
+  token: 'TARI',
+  coinGeckoID: null,
+  id: 'r26ad8',
+  cmcId: 'tari',
+  description: 'Tari enables tinkerers to design new stories and experiences. Someday a mesmerizing and digitally scarce asset or collectible you covet will unlock new business opportunities for the creator and a new world of possibilities for you. This asset will be issued on Tari. Tari is built in Rust and open source.',
+  discord: 'tari',
+  investors: [],
+  linkedin: 'https://linkedin.com/company/tarilabs',
+  duneQueries: null,
+  foundingYear: '2017',
+  blog: 'https://tari.substack.com/',
+  github: 'https://github.com/tari-project',
+  telegram: 'tariproject',
+  twitter: 'https://twitter.com/tari',
+  url: 'https://tari.com ',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Seed',
+      url: 'https://www.coincarp.com/fundraising/tari-seed/',
+    },
+  ],
+} satisfies AdapterProject
