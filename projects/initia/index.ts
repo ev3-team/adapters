8922908ff08820686adabce60b4044e3d7af0052
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:55:15 -0500

    Add projects/initia

---
+++ projects/initia/index.ts
@@ -0,0 +1,31 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Initia',
+  ninja: 'meta-light',
+  subcategories: null,
+  chain: null,
+  category: 'BLOCKCHAIN_INFRA',
+  token: null,
+  coinGeckoID: null,
+  id: 'x0l3lh',
+  cmcId: null,
+  description: 'A Network for Interwoven Rollups',
+  discord: 'initia',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/initia-labs',
+  duneQueries: null,
+  foundingYear: '2023',
+  blog: 'https://medium.com/@initiafdn',
+  github: 'https://github.com/initia-labs',
+  telegram: null,
+  twitter: 'https://twitter.com/initiafdn',
+  url: 'https://initia.xyz',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Seed',
+      url: 'https://techcrunch.com/2024/02/27/initia-raises-7-5m-seed-money-to-ease-blockchain-development',
+    },
+  ],
+} satisfies AdapterProject
