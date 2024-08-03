64ee71cf85963e526441a75784b716c5cab709b0
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:54:03 -0500

    Add projects/everclear

---
+++ projects/everclear/index.ts
@@ -0,0 +1,35 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Everclear',
+  ninja: 'meta-light',
+  subcategories: null,
+  chain: 'ETHEREUM',
+  category: 'BLOCKCHAIN_INFRA',
+  token: 'NEXT',
+  coinGeckoID: 'everclear',
+  id: '59hrvg',
+  cmcId: '27976',
+  description: 'Everclear coordinates the global settlement of liquidity between chains, solving fragmentation for modular blockchains. Previously incorporated as Connext',
+  discord: 'everclear',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/everclearorg',
+  duneQueries: null,
+  foundingYear: '2017',
+  blog: 'https://blog.everclear.org/',
+  github: 'https://github.com/everclearorg',
+  telegram: null,
+  twitter: 'https://twitter.com/everclearorg',
+  url: 'https://www.everclear.org',
+  verified: false,
+  fundraises: [
+    {
+      roundType: '',
+      url: 'https://cointelegraph.com/news/vc-roundup-investors-funding-crypto-blockchain-startups',
+    },
+    {
+      roundType: '',
+      url: 'https://fortune.com/crypto/2023/06/14/connext-labs-raises-web3-polychain',
+    }
+  ],
+} satisfies AdapterProject
