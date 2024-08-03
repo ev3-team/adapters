9acc62016606354608620e78f16588da6565dd74
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:56:32 -0500

    Add projects/moonchain

---
+++ projects/moonchain/index.ts
@@ -0,0 +1,31 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Moon Chain',
+  ninja: 'meta-light',
+  subcategories: 'L1/L2',
+  chain: 'ETHEREUM',
+  category: 'NATIVE',
+  token: 'MXC',
+  coinGeckoID: 'moonchain',
+  id: 'pttqmy',
+  cmcId: '3628',
+  description: 'MXC fork of Taiko. IOT centric L3 on Arbitrum',
+  discord: '4vrJyhX',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/mxc-foundation',
+  duneQueries: null,
+  foundingYear: '2019',
+  blog: 'https://www.mxc.org/blog',
+  github: 'https://github.com/mxc-foundation',
+  telegram: 'https://t.me/mxcfoundation',
+  twitter: 'https://x.com/mxcfoundation',
+  url: 'https://www.moonchain.com',
+  verified: false,
+  fundraises: [
+    {
+      roundType: '',
+      url: 'https://www.coincarp.com/fundraising/mxc-strategic/',
+    },
+  ],
+} satisfies AdapterProject
