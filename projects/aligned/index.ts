e7037e7c19ac5c34786a4dd4c8ae6654eaa73a5d
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:49:25 -0500

    Add projects/aligned

---
+++ projects/aligned/index.ts
@@ -0,0 +1,31 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Aligned',
+  ninja: 'meta-light',
+  subcategories: null,
+  chain: 'n/a',
+  category: 'NOT_DEPIN',
+  token: null,
+  coinGeckoID: null,
+  id: 'm3i0pk',
+  cmcId: null,
+  description: 'Customized LLM Development on Flexible GPU Platforms.',
+  discord: null,
+  investors: [],
+  linkedin: 'https://linkedin.com/company/alignedco',
+  duneQueries: null,
+  foundingYear: '2021',
+  blog: null,
+  github: null,
+  telegram: null,
+  twitter: null,
+  url: 'https://aligned.co',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Seed',
+      url: 'https://blockworks.co/news/aligned-raises-34m-to-grow-defi-and-web3-infrastructure',
+    }
+  ],
+} satisfies AdapterProject
