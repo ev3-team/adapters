f4275989eb476f4ab63fe7a123a8d781cdf392b3
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:57:08 -0500

    Add projects/plume

---
+++ projects/plume/index.ts
@@ -0,0 +1,31 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Plume',
+  ninja: 'meta-light',
+  subcategories: 'L1/L2',
+  chain: 'ETHEREUM',
+  category: 'BLOCKCHAIN_INFRA',
+  token: null,
+  coinGeckoID: null,
+  id: 'l9kem5',
+  cmcId: null,
+  description: 'Plume Network is the first modular RWA L2 built to bring any real world asset onchain',
+  discord: 'plume-network',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/plume-network/',
+  duneQueries: null,
+  foundingYear: null,
+  blog: 'https://www.plumenetwork.xyz/blog',
+  github: null,
+  telegram: 'https://t.me/plumenetwork_community',
+  twitter: 'https://x.com/plumenetwork?s=20',
+  url: 'https://plumenetwork.xyz',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Seed',
+      url: 'https://www.theblock.co/post/296363/rwa-layer-2-plume-network-seed-funding?utm_source=telegram1&utm_medium=social',
+    },
+  ],
+} satisfies AdapterProject
