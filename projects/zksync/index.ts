dfc65f15e99775f4646d827ff376ff3d440a47b4
meta-lite <nick@myceliumnetworks.com>
2024-08-03 13:05:37 -0500

    Add projects/zksync

---
+++ projects/zksync/index.ts
@@ -0,0 +1,43 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'zkSync',
+  ninja: 'meta-light',
+  subcategories: 'L1/L2',
+  chain: 'ETHEREUM',
+  category: 'COMPUTE',
+  token: 'ZK',
+  coinGeckoID: 'zksync',
+  id: 'vbgaz8',
+  cmcId: '24091',
+  description: 'ZKsync is cutting-edge zero-knowledge (ZK) technology to scale Ethereum and bring crypto to the mainstream — reaching millions of developers and billions of people in need of a technological solution for achieving progress and prosperity.',
+  discord: 'W44KrYE',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/zksync-network/',
+  duneQueries: null,
+  foundingYear: '2019',
+  blog: 'https://zksync.mirror.xyz/',
+  github: 'https://github.com/matter-labs',
+  telegram: 'https://t.me/zksync',
+  twitter: 'https://twitter.com/zksync',
+  url: 'https://zksync.io',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Seed', 
+      url: 'https://www.prnewswire.com/news-releases/matter-labs-raises-2m-to-bring-scalability-to-ethereum-with-zero-knowledge-proofs-300923258.html', 
+    },
+    {
+      roundType: 'Series A', 
+      url: 'https://www.investing.com/news/cryptocurrency-news/zksync-developer-matter-labs-raises-200m-reserves-two-thirds-of-its-future-token-supply-to-ecosystem-2946891', 
+    },
+    {
+      roundType: 'Series B', 
+      url: 'https://blog.matter-labs.io/funding-ea89c1fa731e', 
+    },
+    {
+      roundType: 'Series C', 
+      url: 'https://techcrunch.com/2022/11/16/matter-labs-the-company-behind-zksync-raises-200-million-to-scale-ethereum/', 
+    },
+  ],
+} satisfies AdapterProject
