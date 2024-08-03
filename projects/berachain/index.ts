17a7fca3ed7315f46bec1616f8d0ad3220934a5b
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:52:07 -0500

    Add projects/berachain

---
+++ projects/berachain/index.ts
@@ -0,0 +1,35 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Berachain',
+  ninja: 'meta-light',
+  subcategories: 'L1/L2',
+  chain: 'ETHEREUM',
+  category: 'NOT_DEPIN',
+  token: 'BERA',
+  coinGeckoID: 'berachain-bera',
+  id: 'wll7cx',
+  cmcId: '24647',
+  description: 'An EVM-identical L1 turning liquidity into securitypowered by Proof Of Liquidity.',
+  discord: 'berachain',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/berachain/',
+  duneQueries: null,
+  foundingYear: '2023',
+  blog: 'https://blog.berachain.com/',
+  github: 'https://github.com/berachain',
+  telegram: 'BerachainPortal',
+  twitter: 'https://x.com/berachain',
+  url: 'https://berachain.com',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Series A',
+      url: 'https://www.coindesk.com/business/2023/04/20/defi-focused-layer-1-berachain-raises-42m-series-a-at-42069m-valuation',
+    },
+    {
+      roundType: 'Series B',
+      url: 'https://www.theblock.co/post/288066/layer-1-blockchain-developer-berachain-raises-100-million-in-series-b-funding',
+    },
+  ],
+} satisfies AdapterProject
