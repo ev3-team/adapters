ef8e7a735f100b981d64b1f8a13159fd10d1b937
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:51:06 -0500

    Add projects/avail

---
+++ projects/avail/index.ts
@@ -0,0 +1,35 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Avail',
+  ninja: 'meta-light',
+  subcategories: 'ZK',
+  chain: 'NATIVE',
+  category: 'BLOCKCHAIN_INFRA',
+  token: 'AVAIL',
+  coinGeckoID: 'avail',
+  id: 'zrt6w4',
+  cmcId: '32376',
+  description: 'Avail is a Web3 infrastructure layer that allows modular execution layers to scale and interoperate in a trust minimized way.',
+  discord: 'y6fHnxZQX8',
+  investors: [],
+  linkedin: 'https://linkedin.com/company/availproject',
+  duneQueries: null,
+  foundingYear: '2023',
+  blog: 'https://blog.availproject.org/',
+  github: 'https://github.com/availproject',
+  telegram: 'AvailCommunity',
+  twitter: 'https://twitter.com/AvailProject',
+  url: 'https://availproject.org',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Seed',
+      url: 'https://blog.availproject.org/avail-raises-27m-to-accelerate-the-unification-of-web3/',
+    },
+    {
+      roundType: 'Series A',
+      url: 'https://blog.availproject.org/avail-raises-a-total-of-75m-to-build-unification-layer-for-web3/',
+    }
+  ],
+} satisfies AdapterProject
