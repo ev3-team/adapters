aa234a791698547a9fa6381a55a99e1ccfd34418
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:51:37 -0500

    Add projects/aztec

---
+++ projects/aztec/index.ts
@@ -0,0 +1,39 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Aztec',
+  ninja: 'meta-light',
+  subcategories: 'L1/L2',
+  chain: 'ETHEREUM',
+  category: 'null',
+  token: null,
+  coinGeckoID: null,
+  id: 'avu7pz',
+  cmcId: null,
+  description: 'Aztec is a privacy-first Layer 2 on Ethereum. It enables a critical dimension of programmable digital money that has heretofore been ignored: privacy.',
+  discord: 'aztec',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/aztec-protocol/',
+  duneQueries: null,
+  foundingYear: '2017',
+  blog: 'https://aztec.network/blog',
+  github: 'https://github.com/AztecProtocol',
+  telegram: null,
+  twitter: 'https://twitter.com/aztecnetwork',
+  url: 'https://aztec.network',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Seed',
+      url: 'https://www.finsmes.com/2018/11/aztec-raises-2-1m-seed-funding.html',
+    },
+    {
+      roundType: 'Series A',
+      url: 'https://www.coindesk.com/business/2021/12/16/ethereum-privacy-startup-aztec-raises-17m-in-paradigm-led-series-a.',
+    },
+    {
+      roundType: 'Series B',
+      url: 'https://cointelegraph.com/news/aztec-network-secures-100-million-in-series-b-funding-led-by-a16z',
+    }
+  ],
+} satisfies AdapterProject
