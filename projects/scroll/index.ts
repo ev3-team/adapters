3961a524d7d2c92f890c8b591db33dc5d0426885
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:57:40 -0500

    Add projects/scroll

---
+++ projects/scroll/index.ts
@@ -0,0 +1,35 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Scroll',
+  ninja: 'meta-light',
+  subcategories: 'L1/L2',
+  chain: 'ETHEREUM',
+  category: 'NATIVE',
+  token: 'SCROLL',
+  coinGeckoID: null,
+  id: '0j3bks',
+  cmcId: '26998',
+  description: 'Scroll is a security-focused scaling solution for Ethereum',
+  discord: 'scroll',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/scroll-io/',
+  duneQueries: null,
+  foundingYear: '2021',
+  blog: 'https://scroll.io/blog',
+  github: 'https://github.com/scroll-tech',
+  telegram: null,
+  twitter: 'https://x.com/Scroll_ZKP',
+  url: 'https://scroll.io',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Series B',
+      url: 'https://www.theblock.co/post/217340/ethereum-scaling-scroll-50-million-funding-round-1-8-billion-valuation',
+    },
+    {
+      roundType: 'Series A',
+      url: 'https://www.theblock.co/post/142800/scroll-raises-ethereum-scaling-project-funding',
+    },
+  ],
+} satisfies AdapterProject
