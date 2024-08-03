40e39f498257d425529b354d0df56f5a49f8f9b1
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:52:21 -0500

    Add projects/blockvolt

---
+++ projects/blockvolt/index.ts
@@ -0,0 +1,26 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Blockvolt',
+  ninja: 'meta-light',
+  subcategories: null,
+  chain: null,
+  category: null,
+  token: null,
+  coinGeckoID: null,
+  id: 'yznlur',
+  cmcId: null,
+  description: 'Get Rewarded for Helping the Planet',
+  discord: null,
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/blockvolt-limited/',
+  duneQueries: null,
+  foundingYear: null,
+  blog: null,
+  github: null,
+  telegram: null,
+  twitter: null,
+  url: 'https://blockvolt.io/',
+  verified: false,
+  fundraises: [],
+} satisfies AdapterProject
