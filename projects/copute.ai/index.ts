d79020ef0bede7e391ceadc02ff985b7fa497abe
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:52:43 -0500

    Add projects/copute.ai

---
+++ projects/copute.ai/index.ts
@@ -0,0 +1,26 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Copute.ai',
+  ninja: 'meta-light',
+  subcategories: null,
+  chain: null,
+  category: null,
+  token: null,
+  coinGeckoID: null,
+  id: 'la5n2y',
+  cmcId: null,
+  description: null,
+  discord: null,
+  investors: [],
+  linkedin: null,
+  duneQueries: null,
+  foundingYear: null,
+  blog: null,
+  github: null,
+  telegram: null,
+  twitter: null,
+  url: 'https://www.copute.ai/',
+  verified: false,
+  fundraises: [],
+} satisfies AdapterProject
