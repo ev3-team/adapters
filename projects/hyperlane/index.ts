855330db4101c5aa7ddd8ff7c11a1dd15df766e6
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:54:53 -0500

    Add projects/hyperlane

---
+++ projects/hyperlane/index.ts
@@ -0,0 +1,31 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Hyperlane',
+  ninja: 'meta-light',
+  subcategories: null,
+  chain: null,
+  category: 'BLOCKCHAIN_INFRA',
+  token: null,
+  coinGeckoID: null,
+  id: 'udbs15',
+  cmcId: null,
+  description: 'Hyperlane is the open interoperability framework to connect anywhere onchain.',
+  discord: 'VK9ZUy3aTV',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/hyperlane-xyz/',
+  duneQueries: null,
+  foundingYear: '2022',
+  blog: 'https://medium.com/hyperlane',
+  github: 'https://github.com/hyperlane-xyz',
+  telegram: null,
+  twitter: 'https://twitter.com/hyperlane_xyz',
+  url: 'https://hyperlane.xyz',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Seed',
+      url: 'https://www.coindesk.com/business/2022/09/22/blockchain-startup-hyperlane-raises-185m-from-crypto-investor-variant',
+    },
+  ],
+} satisfies AdapterProject
