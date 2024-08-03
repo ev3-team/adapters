5efea83720240b34a1e32372a4eae291eea9b77e
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:55:35 -0500

    Add projects/lumio

---
+++ projects/lumio/index.ts
@@ -0,0 +1,31 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Lumio',
+  ninja: 'meta-light',
+  subcategories: null,
+  chain: 'SOLANA',
+  category: 'BLOCKCHAIN_INFRA',
+  token: null,
+  coinGeckoID: null,
+  id: 'aiy4cz',
+  cmcId: null,
+  description: 'The First VM Abstraction Framework',
+  discord: '44QgPFHYqs',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/pontemnetwork/',
+  duneQueries: null,
+  foundingYear: '2016',
+  blog: 'https://substack.com/@lumio',
+  github: 'https://github.com/pontem-network',
+  telegram: 'https://t.me/pontemnetworkchat',
+  twitter: 'https://x.com/lumiofdn',
+  url: 'https://lumio.io',
+  verified: false,
+  fundraises: [
+    {
+      roundType: '',
+      url: 'https://www.theblock.co/post/271967/pontem-raises-6-million-to-enable-ethereum-move-compatible-apps',
+    },
+  ],
+} satisfies AdapterProject
