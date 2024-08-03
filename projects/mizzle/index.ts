8e9b893517b75662f3a1e1c0a4a2cdf4c54a622c
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:56:13 -0500

    Add projects/mizzle

---
+++ projects/mizzle/index.ts
@@ -0,0 +1,31 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Mizzle',
+  ninja: 'meta-light',
+  subcategories: 'Storage',
+  chain: null,
+  category: 'COMPUTE',
+  token: null,
+  coinGeckoID: null,
+  id: 'rmkyjo',
+  cmcId: null,
+  description: 'Mizzle is a DePIN platform redefining distributed compute and storage solutions. Our AI-based DevOps ensures unparalleled ease of use, streamlining infrastructure management for all users. Security is fortified with eBPF technology, providing real-time protection and performance monitoring. We stay ahead with advanced cryptographic techniques like Fully Homomorphic Encryption and Zero Knowledge Proofs, ensuring maximum data privacy and security. Mizzle combines simplicity, robust security, and innovative technology, making it your premier choice for a secure and efficient decentralized infrastructure.',
+  discord: 'WgU5Qmb9yV',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/mizzlecloud',
+  duneQueries: null,
+  foundingYear: '2024',
+  blog: 'https://medium.com/@Mizzle.io',
+  github: null,
+  telegram: 'https://t.me/mizzleio',
+  twitter: 'https://x.com/mizzle_io',
+  url: 'https://mizzle.io',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Seed',
+      url: 'https://medium.com/@Mizzle.io/mizzle-cloud-secures-1m-funding-from-onextel-to-revolutionize-decentralized-infrastructure-fd705b7fdab6',
+    },
+  ],
+} satisfies AdapterProject
