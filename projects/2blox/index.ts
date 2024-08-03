6f7d23f639761c762a87f57f16b11f143a1c8feb
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:49:04 -0500

    Add projects/2blox

---
+++ projects/2blox/index.ts
@@ -0,0 +1,31 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: '2Blox',
+  ninja: 'meta-light',
+  subcategories: 'Mobility',
+  chain: 'PEAQ',
+  category: 'SENSORS',
+  token: null,
+  coinGeckoID: null,
+  id: 'rfqh7a',
+  cmcId: null,
+  description: '2Blox is a sensor network that collects data on the movement of people and vehicles. On Feb 1st, 2024 the company released a statement that it was shutting down. Since, the X.com account has rebranded to @juiceup_network. Details here: https://x.com/juiceup_network/status/1753140887016681567',
+  discord: null,
+  investors: [],
+  linkedin: 'https://linkedin.com/company/2bloxaway',
+  duneQueries: null,
+  foundingYear: '2022',
+  blog: null,
+  github: 'https://jsdelivr.com/package/npm/@2blox/wireless-tools',
+  telegram: null,
+  twitter: 'https://twitter.com/2bloxaway',
+  url: 'https://2blox.io', //out of commission
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Accelerator',
+      url: 'https://pitchbook.com/profiles/company/515546-83#overview',
+    }
+  ],
+} satisfies AdapterProject
