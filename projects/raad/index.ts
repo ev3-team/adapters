42e764739036b7ad636930d58d885fde8ea9ab60
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:57:25 -0500

    Add projects/raad

---
+++ projects/raad/index.ts
@@ -0,0 +1,31 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'RaaD',
+  ninja: 'meta-light',
+  subcategories: null,
+  chain: null,
+  category: 'SENSORS',
+  token: null,
+  coinGeckoID: null,
+  id: 'mgg7s9',
+  cmcId: null,
+  description: 'Raad Labs is a company deploying a distributed network of meteorological sensors, incentivizing individuals and businesses to purchase and host these devices. This network provides hyper-localized data to governments, energy traders, utilities, and insurers to improve decision-making, protect infrastructure, and prevent human suffering.',
+  discord: null,
+  investors: [],
+  linkedin: null,
+  duneQueries: null,
+  foundingYear: '2024',
+  blog: null,
+  github: null,
+  telegram: null,
+  twitter: 'https://x.com/raadlabs',
+  url: 'https://raad.com',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Seed',
+      url: 'https://www.morningstar.com/news/business-wire/20240730466375/raad-labs-announces-225m-funding-round-led-by-coinfund-and-appoints-tj-ragsdale-as-ceo',
+    },
+  ],
+} satisfies AdapterProject
