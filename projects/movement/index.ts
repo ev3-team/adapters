b9739b12b1bf27a5b2ffa7eb5ab57f9a936324c3
meta-lite <nick@myceliumnetworks.com>
2024-08-03 12:56:52 -0500

    Add projects/movement

---
+++ projects/movement/index.ts
@@ -0,0 +1,35 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Movement',
+  ninja: 'meta-light',
+  subcategories: 'L1/L2',
+  chain: 'ETHEREUM',
+  category: 'BLOCKCHAIN_INFRA',
+  token: 'MOVE',
+  coinGeckoID: 'movement',
+  id: '9zen27',
+  cmcId: '32084',
+  description: 'Movement Is a Network Of Modular Move Based Blockchains',
+  discord: 'movementlabsxyz',
+  investors: [],
+  linkedin: 'https://www.linkedin.com/company/movementlabsxyz',
+  duneQueries: null,
+  foundingYear: '2023',
+  blog: 'https://blog.movementlabs.xyz',
+  github: 'https://github.com/movementlabsxyz',
+  telegram: 'https://t.me/movementlabsxyz',
+  twitter: 'https://x.com/movementlabsxyz',
+  url: 'https://movementlabs.xyz',
+  verified: false,
+  fundraises: [
+    {
+      roundType: 'Series A',
+      url: 'https://www.coindesk.com/tech/2024/04/25/movement-labs-raises-38m-for-rollup-based-on-facebooks-move-language',
+    },
+    {
+      roundType: 'Seed',
+      url: 'https://finbold.com/movement-labs-raises-over-3m-in-pre-seed-round-and-releases-movement-sdk-for-web3',
+    },
+  ],
+} satisfies AdapterProject
