b010c0c921d6d7aeb63cdcde3b50eeb4c7f7a547
Zoltan Zakany <zzakany.work@gmail.com>
2024-09-14 10:28:53 +0000

    submitted project added2

---
+++ projects/digitalabs/index.ts
@@ -0,0 +1,27 @@
+import { AdapterProject } from '../types'
+
+export default {
+  name: 'Digitalabs',
+  ninja: null,
+  subcategories: 'GPUs',
+  chain: 'NATIVE',
+  category: 'COMPUTE',
+  token: 'DIL',
+  coinGeckoID: null,
+  id: '3s40pm',
+  description:
+    'Digitalabs is a next-generation decentralized cloud service and AI training platform that provides on-demand, scalable computing power through a globally distributed network of GPU/CPU clusters optimized for AI, gaming, and data-intensive applications.',
+  discord: null,
+  investors: [],
+  linkedin: null,
+  duneQueries: null,
+  foundingYear: null,
+  blog: null,
+  github: null,
+  telegram: null,
+  twitter: 'https://x.com/Digitalabs_AI',
+  url: 'https://digitalabs.ai/',
+  verified: false,
+  fundraises: [],
+  isApp: true,
+} satisfies AdapterProject
